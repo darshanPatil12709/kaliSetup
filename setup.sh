291f716290d7d7d469f30bae942ad9e2d519e42a
darshanPatil12709 <dgaptil339@gmail.com>
2024-07-20 22:26:12 +0100

    sudo update

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 # Update and upgrade the system
 echo "[*] Updating and upgrading the system..."
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 
 # Install additional tools
 echo "[*] Installing additional tools..."
-apt install -y \
+sudo apt install -y \
     gobuster \
     seclists \
     feroxbuster \
@@ -24,47 +24,40 @@ apt install -y \
 
 # Install pip and Python tools
 echo "[*] Installing Python tools..."
-apt install -y python3-pip
+sudo apt install -y python3-pip
 pip3 install updog
 pip3 install impacket
 
 # Create a directory for GitHub repos
-mkdir -p /opt/github
+sudo mkdir -p /opt/github
 
 # Clone useful GitHub repositories
 echo "[*] Cloning useful GitHub repositories..."
 cd /opt/github
 
-git clone https://github.com/carlospolop/PEASS-ng.git
-git clone https://github.com/rebootuser/LinEnum.git
-git clone https://github.com/DominicBreuker/pspy.git
-git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git
-git clone https://github.com/PowerShellMafia/PowerSploit.git
-git clone https://github.com/samratashok/nishang.git
-git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
-git clone https://github.com/danielmiessler/SecLists.git
+sudo git clone https://github.com/carlospolop/PEASS-ng.git
+sudo git clone https://github.com/rebootuser/LinEnum.git
+sudo git clone https://github.com/DominicBreuker/pspy.git
+sudo git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git
+sudo git clone https://github.com/PowerShellMafia/PowerSploit.git
+sudo git clone https://github.com/samratashok/nishang.git
+sudo git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+sudo git clone https://github.com/danielmiessler/SecLists.git
+sudo git clone  https://github.com/dirkjanm/krbrelayx
 
 # Install and setup Golang
 echo "[*] Installing and setting up Golang..."
-apt install -y golang
-echo 'export GOROOT=/usr/lib/go' >> ~/.bashrc
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+sudo apt install golang -y
 
 # Install additional Go tools
 echo "[*] Installing Go tools..."
-go install github.com/OJ/gobuster/v3@latest
-go install github.com/ffuf/ffuf@latest
-
-# Setup Metasploit database
-echo "[*] Setting up Metasploit database..."
-msfdb init
+sudo go install github.com/OJ/gobuster/v3@latest
+sudo go install github.com/ffuf/ffuf@latest
 
 # Final cleanup
 echo "[*] Cleaning up..."
-apt autoremove -y
-apt clean
+sudo apt autoremove -y
+sudo apt clean
 
 echo "[+] Setup complete! Your Kali VM should now be better equipped for pentesting."
 echo "[!] Remember to reboot your system to ensure all changes take effect."
